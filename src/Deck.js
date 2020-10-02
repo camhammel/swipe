93fc4913977bb91a7f09fdd5c1c3b4938bb2b677
Cameron Hammel <cameronhammel@MacBook-Pro.local>
2020-10-02 14:57:09 -0400

    added panResponder

--- src/Deck.js
+++ src/Deck.js
@@ -1,13 +1,31 @@
-import React from "react";
-import { View, Text, Animated } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, Animated, PanResponder } from "react-native";
 
 const Deck = ({ data, renderCard }) => {
+  const [panResponder, setPanResponder] = useState(null);
+
+  useEffect(() => {
+    const panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (event, gesture) => {
+        console.log(gesture);
+      },
+      onPanResponderRelease: () => {},
+    });
+
+    setPanResponder(panResponder);
+  }, []);
+
   function renderCards() {
     return data.map((item) => {
       return renderCard(item);
     });
   }
-  return renderCards();
+  return (
+    <View {...(panResponder != null ? panResponder.panHandlers : null)}>
+      {renderCards()}
+    </View>
+  );
 };
 
 export default Deck;
